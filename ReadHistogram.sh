d00c847141e123935a0135ef93c82acb1c204622
Patrick Reilly <preilly@princeton.edu>
2018-11-18 11:32:53 -0500

    Added the ability to specify temporary directory for sorting to ReadHistogram.sh

--- ReadHistogram.sh
+++ ReadHistogram.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 READFILE=$1
+SORTOPTS=$2
 if [[ $1 =~ .gz$ ]]; then
-   zcat ${READFILE} | awk 'NR%4==2' | sort | uniq -c | sort -k1,1nr
+   zcat ${READFILE} | awk 'NR%4==2' | sort ${SORTOPTS} | uniq -c | sort -k1,1nr
 else
-   awk 'NR%4==2' ${READFILE} | sort | uniq -c | sort -k1,1nr
+   awk 'NR%4==2' ${READFILE} | sort ${SORTOPTS} | uniq -c | sort -k1,1nr
 fi
